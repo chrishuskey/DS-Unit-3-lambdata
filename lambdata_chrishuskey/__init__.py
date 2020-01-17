c99ba102f6d785ac9a225a981eb24a93c5bf51e8
chrishuskey <chrishuskey@users.noreply.github.com>
2020-01-17 00:20:12 -0600

    added module lambdata_chrishuskey including its __init__.py with started/filler datasets and functions to test.

---
+++ lambdata_chrishuskey/__init__.py
@@ -0,0 +1,12 @@
+"""lambdata: A collection of data science helper functions by chrishuskey."""
+
+import numpy as np  # already installed numpy in this project's virtual environment
+import pandas as pd  # already installed pandas in this project's virtual environment
+
+# Sample/filler datasets as a placeholder for now:
+ZEROS = pd.DataFrame(np.zeros(50))
+ONES = pd.DataFrame(np.ones(10))
+
+# Sample/filler functions as a placeholder for now:
+def increment(number):
+    return (number + 1)
