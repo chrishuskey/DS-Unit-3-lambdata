b72c0cf2913a1841f01256a888367abb43da538e
chrishuskey <chrishuskey@users.noreply.github.com>
2020-01-17 00:44:06 -0600

    added df_utils module, which includes some proof-of-concept utility functions.

---
+++ lambdata_chrishuskey/df_utils.py
@@ -0,0 +1,7 @@
+"""df_utils: Utility functions for working specifically with dataframes."""
+
+import pandas as pd
+import numpy as np
+
+# Datasets:
+TEST_DF = pd.DataFrame(np.full((4, 4), 8, dtype='int'))
